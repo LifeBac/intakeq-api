6b54635af4a576603b5e4f5e6c86b0cfb7438ef7
Justin Rahardjo <justindra91@gmail.com>
2020-03-30 20:08:54 -0600

    chore: bootstrap the repo and add build tools

---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  singleQuote: true,
+  arrowParens: 'always',
+  jsxSingleQuote: true,
+  jsxBracketSameLine: true,
+  endOfLine: 'lf',
+};
---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+import typescript from 'rollup-plugin-typescript2';
+import pkg from './package.json';
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+    },
+    {
+      name: '@lifebac/intakeq',
+      file: pkg.browser,
+      format: 'umd',
+      globals: {},
+    },
+  ],
+  external: [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
+  ],
+  plugins: [
+    typescript({
+      typescript: require('typescript'),
+    }),
+  ],
+};
