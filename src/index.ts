2f692cc875f9a052fc7c9d31b1223231fd99c19b
Justin Rahardjo <justindra91@gmail.com>
2020-03-30 20:21:55 -0600

    chore: added eslint

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
-const a = 'asdasd';
+import axios, { AxiosInstance } from 'axios';
 
-export { a };
+class IntakeQApi {
+  protected api: AxiosInstance;
+
+  constructor(apiKey: string) {
+    this.api = axios.create({
+      baseURL: 'https://intakeq.com/api/v1',
+      timeout: 5000,
+      headers: { 'X-Auth-Key': apiKey },
+    });
+  }
+}
+
+export { IntakeQApi };
+
+export default IntakeQApi;
