bcdad74eac7266079c2c760f42808549774c5615
Justin Rahardjo <justindra91@gmail.com>
2020-03-30 20:30:31 -0600

    chore: adjusted eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,9 @@
 module.exports = {
-  extends: 'airbnb-typescript-prettier',
+  extends: ['airbnb-typescript-prettier'],
+  rules: {
+    // prettier
+    'prettier/prettier': 'off',
+    'no-useless-constructor': 'off',
+    'import/prefer-default-export': 'off',
+  },
 };
