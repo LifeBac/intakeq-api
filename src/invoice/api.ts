cbf23c4789003a0d87832db0ee37c4700bf5f654
Patrick <patrick@lifebac.com>
2020-12-30 12:47:19 -0800

    chore(invoices): Update Webhook Options

--- src/invoice/api.ts
+++ src/invoice/api.ts
@@ -1,43 +1,36 @@
-import {AxiosInstance} from 'axios';
-import {
-    Invoice,
-    ListInvoicesRequest
-} from './interfaces';
-import {Appointment} from "..";
+import { AxiosInstance } from 'axios';
+import { Invoice, ListInvoicesRequest } from './interfaces';
 
 const ENDPOINT = 'invoices';
 
 export class InvoiceApi {
-    constructor(private api: AxiosInstance) {
-    }
+  constructor(private api: AxiosInstance) {}
 
   /**
    * Use this endpoint to query for matching invoices.
    * @param params - The request object for querying invoices
    */
-  async listInvoices(
-        params: ListInvoicesRequest
-    ): Promise<Invoice[]> {
-        const urlParams = [];
+  async listInvoices(params: ListInvoicesRequest): Promise<Invoice[]> {
+    const urlParams = [];
 
-        Object.keys(params).forEach((key) => {
-            if (!params[key]) return;
-            urlParams.push(`${key}=${params[key]}`)
-        });
+    Object.keys(params).forEach((key) => {
+      if (!params[key]) return;
+      urlParams.push(`${key}=${params[key]}`);
+    });
 
-        const res = await this.api.get<Invoice[]>(
-            `${ENDPOINT}?${urlParams.join('&')}`
-        );
+    const res = await this.api.get<Invoice[]>(
+      `${ENDPOINT}?${urlParams.join('&')}`
+    );
 
-        return res.data;
-    }
+    return res.data;
+  }
 
   /**
    * Loads a single invoice from it's ID.
    * @param invoiceId The ID of the invoice to load
    */
   async getInvoice(invoiceId): Promise<Invoice> {
-      const res = await this.api.get<Invoice>(`${ENDPOINT}/${invoiceId}`);
-      return res.data;
-    }
+    const res = await this.api.get<Invoice>(`${ENDPOINT}/${invoiceId}`);
+    return res.data;
+  }
 }
